902ec59b3dd1b2c11ac526c560d58b66f6c9c87f
Dev-Sameer-Khan <khansameer84233@gmail.com>
2025-03-23 00:20:40 +0530

    Remove unused logo assets and enhance Loader component with GSAP animations

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,109 @@
-import React from 'react'
+import gsap from "gsap";
+import React, { useEffect } from "react";
 
 const Loader = () => {
-  return (
-    <section className='w-full h-screen bg-black text-white flex items-center justify-end relative'>
-        <div className='logo w-[30%] opacity-100 select-none -translate-x-1/2 -translate-y-1/2 transform absolute top-1/2 left-1/2'>
-        <img className='w-full h-full object-cover' src="./src/assests/images/NimbusLogo.png" alt="" />
-        </div>
-    </section>
-  )
-}
+  useEffect(() => {
+    gsap.fromTo(
+      ".logo",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration : 10,
+        ease : "none"
+      }
+    );
+  }, []);
 
-export default Loader
+  return (
+    <section className="w-full h-screen bg-black text-white flex justify-end relative p-6">
+      <div className="logo w-[20%] opacity-100 select-none -translate-x-1/2 -translate-y-1/2 transform absolute top-1/2 left-1/2">
+        <img
+          className="w-full h-full object-cover"
+          src="https://res.cloudinary.com/dbgzq41x2/image/upload/v1742665464/NimbusLogo_ygg9ki.png"
+          alt=""
+        />
+      </div>
+      <div className="group flex">
+      <div>
+        0%
+      </div>
+      <div className="lines h-full flex items-center justify-end flex-col gap-2">
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+        <span className="h-[1px] w-12 inline-block bg-white"></span>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Loader;
