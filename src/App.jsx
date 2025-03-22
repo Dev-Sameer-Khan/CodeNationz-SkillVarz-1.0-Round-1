a3eda7fcb919c87cbbb78b8c0370a21eaa19632b
Dev-Sameer-Khan <khansameer84233@gmail.com>
2025-03-22 22:30:07 +0530

    Initialize React project with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from './components/Loader'
+
+const App = () => {
+  return (
+    <>
+    <Loader/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <section className='w-full h-screen bg-black text-white'>Loader</section>
+  )
+}
+
+export default Loader
